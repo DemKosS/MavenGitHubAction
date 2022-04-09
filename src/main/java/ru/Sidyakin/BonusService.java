07929f6285345aa6c09ac1c56dc4f1c01efc0c1b
DemKosS <sidyakinkn@gmail.com>
2022-04-09 12:47:52 +0300

    Test

---
+++ src/main/java/ru/Sidyakin/BonusService.java
@@ -0,0 +1,10 @@
+package ru.Sidyakin;
+
+public class BonusService {
+    public void addBonus(){
+        Main main = new Main();
+
+
+
+    }
+}
---
+++ src/test/java/ru/Sidyakin/BonusServiceTest.java
@@ -0,0 +1,12 @@
+package ru.Sidyakin;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BonusServiceTest {
+
+    @Test
+    void addBonus() {
+    }
+}
