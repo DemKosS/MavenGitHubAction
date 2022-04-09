2829fe7f04eadd45d7167e84e306378661a992d0
DemKosS <sidyakinkn@gmail.com>
2022-04-09 12:32:32 +0300

    fix main

--- src/main/java/ru/Sidyakin/Main.java
+++ src/main/java/ru/Sidyakin/Main.java
@@ -4,6 +4,5 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hell world");
         System.out.println("We add gitHubAction");
-        var1 = 1;
     }
 }
