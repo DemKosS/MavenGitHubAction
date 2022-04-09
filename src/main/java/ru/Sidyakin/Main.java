68f9a9c34f203be2df050b032717af66dd65ac0e
DemKosS <sidyakinkn@gmail.com>
2022-04-09 12:02:27 +0300

    add new code

--- src/main/java/ru/Sidyakin/Main.java
+++ src/main/java/ru/Sidyakin/Main.java
@@ -3,5 +3,7 @@ package ru.Sidyakin;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hell world");
+        System.out.println("We add gitHubAction");
+        var1 = 1;
     }
 }
